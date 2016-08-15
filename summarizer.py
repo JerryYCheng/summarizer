d6c8ae8c4bb7992bc57e98ce32248121119e8e5a
Jerry Cheng <Spectedrill@gmail.com>
2016-08-15 01:11:55 -0700

    added documentation

--- summarizer.py
+++ summarizer.py
@@ -41,8 +41,11 @@ Any ground assaults on the cities are still months away, but Iraqi and Kurdish r
 Forces aligned with the Kurdish regional government said Sunday in a statement they have retaken five villages east of Mosul. Iraqi government forces have been closing in on the city from the south.
 
 """
-
+### Summarizer class has functions to summzarize a given text to a specified number of sentences or by a specified percentage. It also has functions that
+### use common word filtering and can present results in both chronological order and in order of most to least significant.
 class summarizer:
+	### Initializes several important structures that contain data about the text, including the word count, filtered word count, sentence list,
+	### number of words, sentence scores, and filtered sentences scores.
 	def __init__(self):
 		self.common_words = ["the", "be", "am", "is", "are", "being", "was", "were", "been", "to", "of", "and", "a", "in", "that", "have",
 							 "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they"
@@ -61,20 +64,23 @@ class summarizer:
 		self.filtered_word_count = self.filter_word_count()
 		self.filtered_sentence_scores = self.score(self.sentence_list, self.filtered_word_count)
 
+	### Takes a word and removes any puntuation from it and reduces it to all lower case. Returns the result
 	def strip_word(self, word):
 		trans = str.maketrans({c: None for c in string.punctuation})
 		new_word = word.translate(trans)
 		return new_word.lower()
 
-
+	### Given a body of text, splits it into sentences using the nltk punkt tokenizer. Returns a list containing all the sentences.
 	def split_text(self, text):
-		#text = "Hello, it's me. I was wondering if after all these years you'd like to meet."
 		text = text.replace('.\n', '. ')
 		text = text.replace('\n', '. ')
 		sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 		list_of_sentences = sentence_tokenizer.tokenize(text.strip())
 		return list_of_sentences
 
+	### Given a body of text, splits it into individual words and find the stem of each word. Then it counts how many times each stem appears
+	### in the text and puts that into a dictionary. Returns this dictionary, word_count
+	### Also returns a mapping of each word stem to its lemmatization as a dictionary, word_map
 	def count_words(self, text):
 		#text = "Hello, it's me! I was wondering if after all these years you'd like to meet. Hello can you hear me too."
 		word_count = {}
@@ -91,6 +97,8 @@ class summarizer:
 				word_map[word_stem] = word_lem
 		return word_count, word_map
 
+	### Given a list of sentences and scores for each word, calculates the score for each sentence by adding all the words in the sentence.
+	### Returns a dictionary mapping each sentence to its calculated score.
 	def score(self, sentences, word_score):
 		sent_scores = {}
 		for sentence in sentences:
@@ -106,6 +114,7 @@ class summarizer:
 			sent_scores[sentence] = score
 		return sent_scores
 
+	### Goes through a copy of the word_count dictionary and removes all common words from it, returning the new filtered dictionary
 	def filter_word_count(self):
 		filtered_word_count = {}
 		for word in self.word_count:
@@ -113,18 +122,30 @@ class summarizer:
 				filtered_word_count[word] = self.word_count[word]
 		return filtered_word_count
 
+	### Given a number of sentences, num, prints a summary of the text that is the given number of sentences long with the sentences in the
+	### summary being ordered from most to least important
 	def ordered_summarize(self, num):
 		self.ordered_summarize_helper(self.sentence_scores, num)
 
+	### Given a number of sentences, num, prints a summary of the text that is the given number of sentences long with the sentences in the
+	### summary being ordered from most to least important. This summary does not consider common words when calculating the sentence scores.
 	def filtered_ordered_summarize(self, num):
 		self.ordered_summarize_helper(self.filtered_sentence_scores, num)
 
+	### Given a percentage, percent, prints a summary of the text that roughly reduces the length of the text by the given percentage with the 
+	### sentences in the summary being ordered from most to least important
 	def ordered_summarize_p(self, percent):
 		self.ordered_summarize_p_helper(self.sentence_scores, percent)
 
+	### Given a percentage, percent, prints a summary of the text that roughly reduces the length of the text by the given percentage with the 
+	### sentences in the summary being ordered from most to least important. This summary does not consider common words when calculating the 
+	### sentence scores.
 	def filtered_ordered_summarize_p(self, percent):
 		self.ordered_summarize_p_helper(self.filtered_sentence_scores, percent)
 
+	### Helper function that takes in a dictionary of sentence scores, sentence_scores, and a number of sentences, num, and prints out
+	### the summary of the text that is the given number of sentences long in order of most to least important, using the sentence scores
+	### in the given dictionary.
 	def ordered_summarize_helper(self, sentence_scores, num):
 		scores = dict(sentence_scores)
 		try:
@@ -138,6 +159,9 @@ class summarizer:
 		except IndexError:
 			print("ERROR: Article does not have " + str(num) + " sentences. Article has " + str(len(self.sentence_scores)) + " sentences.")
 
+	### Helper function that takes in a dictionary of sentence scores, sentence_scores, and a percentage, percent, and prints out
+	### the summary that reduces the text by roughly the percentage given, with sentences in order of most to least important, using the sentence scores
+	### in the given dictionary.
 	def ordered_summarize_p_helper(self, sentence_scores, percent):
 		if percent >= 100 or percent < 10:
 			print("ERROR: Percentage must be a number from 10 and 99")
@@ -154,18 +178,29 @@ class summarizer:
 				total_words += len(best.split())
 			print("Article reduced by " + str(1 - (total_words / self.number_of_words)) + "%")
 
+	### Given a number of sentences, num, prints out a summary of the text that is the given number of sentences, with the sentences in the same order
+	### they appear in the text.
 	def summarize(self, num):
 		self.summarize_helper(self.sentence_scores, num)
 
+	### Given a number of sentences, num, prints out a summary of the text that is the given number of sentences, with the sentences in the same order
+	### they appear in the text. This summary does not consider common words when calculating the sentence scores.
 	def filtered_summarize(self, num):
 		self.summarize_helper(self.filtered_sentence_scores, num)
 
+	### Given a percentage, percent, prints out a summary that reduces the text roughly by the given percentage, with the sentences in the same order
+	### they appear in the text.
 	def summarize_p(self, percent):
 		self.summarize_p_helper(self.sentence_scores, percent)
 
+	### Given a percentage, percent, prints out a summary that reduces the text roughly by the given percentage, with the sentences in the same order
+	### they appear in the text. This summary does not consider common words when calculating the sentence scores.
 	def filtered_summarize_p(self, percent):
 		self.summarize_p_helper(self.filtered_sentence_scores, percent)
 
+	### Helper function that takes in a dictionary of sentence scores, sentence_scores, and a number of sentences, num, and prints out
+	### the summary of the text that is the given number of sentences long in chronological order, using the sentence scores
+	### in the given dictionary.
 	def summarize_helper(self, sentence_scores, num):
 		best_sentences = []
 		scores = dict(sentence_scores)
@@ -184,6 +219,9 @@ class summarizer:
 		except IndexError:
 			print("ERROR: Article does not have " + str(num) + " sentences. Article has " + str(len(self.sentence_scores)) + " sentences.")
 
+	### Helper function that takes in a dictionary of sentence scores, sentence_scores, and a percentage, percent, and prints out
+	### the summary that reduces the text by roughly the percentage given, with sentences in chronological order, using the sentence scores
+	### in the given dictionary.
 	def summarize_p_helper(self, sentence_scores, percent):
 		if percent >= 100 or percent < 10:
 			print("ERROR: Percentage must be a number from 10 and 99")
@@ -205,19 +243,21 @@ class summarizer:
 					best_sentences.remove(sentence)
 			print("Article reduced by " + str(1 - (total_words / self.number_of_words)) + "%")
 
-
+	### Given a number, num, prints out that many of the top/most frequent words in the text.
 	def print_keywords(self, num):
 		try:
 			self.print_keywords_helper(self.word_count, num)
 		except IndexError:
 			print("ERROR: Article does not have " + str(num) + " distinct words. Article has " + str(len(words_copy)) + " distinct words.")
 
+	### Given a number, num, prints out that many of the top/most frequent words in the text, ignoring any words that are common English words.
 	def print_filtered_keywords(self, num):
 		try:
 			self.print_keywords_helper(self.filtered_word_count, num)
 		except IndexError:
 			print("ERROR: Article does not have " + str(num) + " distinct non-common words. Article has " + str(len(filtered_words_copy)) + " distinct non-common words.")
 
+	### Given a dictionary, word_count, and a number, num, finds the top num words in the dictionary and prints them along with their scores.
 	def print_keywords_helper(self, word_count, num):
 		words_copy = dict(word_count)
 		if num > len(words_copy):
